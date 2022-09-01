a3e2329773378ecbe5d46beea444ee34a6aa2842
Ahmad Iqbal <secondemail1412@gmail.com>
2022-09-01 15:29:27 +0700

    fix header

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Header from './components/Header/Header';
 import ShortLink from './components/ShortLink/ShortLink';
 import Features from './components/Fetaures/Features';
 import Footer from './components/Footer/Footer';
-import Navigation from './components/Navbar/Navigation';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* <Navigation /> */}
       <Header />
       <ShortLink />
       <Features />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,6 @@ import classes from "./Navbar.module.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 const NavBar = () => {
   return (
@@ -24,25 +23,6 @@ const NavBar = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* <Navbar expand="lg">
-        <div className={classes.navbar}>
-          <Navbar.Brand>
-            <h1>Shortly</h1>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <div className={classes["navbar-nav"]}>
-              <Nav.Link href="#">Features</Nav.Link>
-              <Nav.Link href="#">Pricing</Nav.Link>
-              <Nav.Link href="#">Resources</Nav.Link>
-            </div>
-            <div className={classes["navbar-action"]}>
-              <button className={classes["navbar-login"]}>Login</button>
-              <button className={classes["navbar-signup"]}>Sign Up</button>
-            </div>
-          </Navbar.Collapse>
-        </div>
-      </Navbar> */}
     </>
   );
 };
